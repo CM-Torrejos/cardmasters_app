c72fcb30705dde40b4d3a19257f18ca50a3a702a
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-26 01:17:58 +0000

    Create Damages and Returns DocType

---
+++ cardmasters_app/cardmasters_app/doctype/damages_and_returns/test_damages_and_returns.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Shan Torrejos and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDamagesandReturns(FrappeTestCase):
+	pass
