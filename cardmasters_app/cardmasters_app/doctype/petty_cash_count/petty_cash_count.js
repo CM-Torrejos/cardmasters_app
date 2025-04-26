9b70c42aa697902f23de73a4f6f566a6fbda0296
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-26 02:02:17 +0000

    Create Petty Cash Count DocType

---
+++ cardmasters_app/cardmasters_app/doctype/petty_cash_count/petty_cash_count.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Shan Torrejos and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Petty Cash Count", {
+// 	refresh(frm) {
+
+// 	},
+// });
