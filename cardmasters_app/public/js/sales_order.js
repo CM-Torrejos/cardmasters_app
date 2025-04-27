4c2a2658e099e532a735ec314895b5665a44a45c
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-27 17:09:03 +0000

    Client Script (Sales Order): Add Progress to display work order status. TODO: Optimize to use API

--- cardmasters_app/public/js/sales_order.js
+++ cardmasters_app/public/js/sales_order.js
@@ -25,3 +25,46 @@ frappe.ui.form.on('Sales Order', {
     	}
 	}
 });
+
+// TODO: Optimize Script
+// Show Work Order Progress
+frappe.ui.form.on('Sales Order', {
+	refresh: function(frm) {
+    	// Ensure the Sales Order is saved before fetching Work Orders
+    	if (!frm.doc.__islocal) {
+        	// frappe.show_alert("Fetching Work Orders..."); // Debugging message
+
+        	frappe.call({
+            	method: 'frappe.client.get_list',
+            	args: {
+                	doctype: 'Work Order',
+                	filters: { sales_order: frm.doc.name },
+                	fields: ['name', 'status', 'production_item', 'custom_production_type']
+            	},
+            	callback: function(response) {
+                	// frappe.show_alert("Work Orders fetched: " + response.message.length); // Debug message
+                	console.log(response.message);
+
+                	if (response.message.length > 0) {
+                    	let html = '<table class="table table-bordered"><tr><th>Work Order</th><th>Item</th><th>Production Type</th><th>Progress</th></tr>';
+                    	response.message.forEach(wo => {
+                        	html += `<tr>
+                                    	<td><a href="/app/work-order/${wo.name}" target="_blank">${wo.name}</a></td>
+                                    	<td>${wo.production_item}</td>
+                                    	<td>${wo.custom_production_type || 'N/A'}</td>
+                                    	<td>${wo.status}</td>
+                                	</tr>`;
+                    	});
+                    	html += '</table>';
+                    	frm.fields_dict['custom_progress_summary'].$wrapper.html(html);
+                	} else {
+                    	frm.fields_dict['custom_progress_summary'].$wrapper.html("<p>No Work Orders found.</p>");
+                	}
+            	}
+        	});
+    	} else {
+        	frappe.show_alert("Sales Order is not yet saved. Work Orders will load after saving.");
+        	frm.fields_dict['custom_progress_summary'].$wrapper.html("<p>Save the Sales Order to view Work Orders.</p>");
+    	}
+	}
+});
