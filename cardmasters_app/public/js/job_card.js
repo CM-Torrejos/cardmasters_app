00ec00f9072b30b04533e18b322b568d09d6e920
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-27 16:54:37 +0000

    Client Script (Job Card): Add Create Petty Cash Request button in Job Card

--- cardmasters_app/public/js/job_card.js
+++ cardmasters_app/public/js/job_card.js
@@ -1,3 +1,4 @@
+// Validate finished QTY before submission
 frappe.ui.form.on("Job Card", {
 	validate: function(frm) {
     	if (frm.doc.status === "Completed") {
@@ -5,5 +6,20 @@ frappe.ui.form.on("Job Card", {
             	frappe.throw(`Completed quantity (${frm.doc.total_completed_qty}) must match the total quantity (${frm.doc.total_qty}) before completing this job.`);
         	}
     	}
+	}
+});
+
+// Create Petty Cash Request
+frappe.ui.form.on('Job Card', {
+	refresh: function(frm) {
+    	// Remove ONLY the "Material Request" button from the "Create" menu
+    	frm.remove_custom_button('Material Request');
+
+    	// Add a standalone button (not inside the dropdown)
+    	frm.add_custom_button('Create Petty Cash Request', () => {
+        	frappe.new_doc('Petty Cash Request', {
+            	job_card: frm.doc.name
+        	});
+    	});
 	}
 });
