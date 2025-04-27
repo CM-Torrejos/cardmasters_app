cd85ca1484e616282fd565eddf01068794bb2da2
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-27 16:56:45 +0000

    Client Script (Sales Order): Add Create Artist Sheet button

--- cardmasters_app/public/js/sales_order.js
+++ cardmasters_app/public/js/sales_order.js
@@ -1,3 +1,4 @@
+// Create Petty Cash Request
 frappe.ui.form.on('Sales Order', {
 	refresh(frm) {
     	if (!frm.is_new()) {
@@ -6,6 +7,21 @@ frappe.ui.form.on('Sales Order', {
                 	sales_order: frm.doc.name
             	});
         	}, 'Create'); // This adds it under the 'Create' dropdown
+    	}
+	}
+});
+
+// Create Artist Sheet
+frappe.ui.form.on('Sales Order', {
+	refresh: function(frm) {
+    	if (!frm.doc.__islocal) {
+        	// Add the button to the "Create" dropdown
+        	frm.add_custom_button(__('Create Artist Sheet'), function() {
+            	frappe.new_doc('Artist Sheet', {
+                	sales_order: frm.doc.name,
+                	artist: frm.doc.custom_artist
+            	});
+        	}, __('Create')); // The third parameter places it in the "Create" dropdown
     	}
 	}
 });
