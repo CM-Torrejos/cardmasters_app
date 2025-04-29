6d23f0c503e7626e97289781845879702a976925
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-04-29 02:08:12 +0000

    Create 'Work Order' button to create a Work Order against Damages and Returns DocType

--- cardmasters_app/public/js/damages_and_returns.js
+++ cardmasters_app/public/js/damages_and_returns.js
@@ -7,7 +7,21 @@ frappe.ui.form.on('Damages and Returns', {
             custom_damages_and_returns: frm.doc.name,
             purpose: 'Material Issue'
           });
-        }); // Group it under 'Create' if you like
+        }, 'Create'); // Group it under 'Create' if you like
+      }
+    }
+  });
+  
+
+frappe.ui.form.on('Damages and Returns', {
+    refresh: function(frm) {
+      // Only show the button if the document is saved
+      if (!frm.is_new()) {
+        frm.add_custom_button('Work Order', () => {
+          frappe.new_doc('Work Order', {
+            custom_damages_and_returns: frm.doc.name,
+          });
+        }, 'Create'); // Group it under 'Create' if you like
       }
     }
   });
