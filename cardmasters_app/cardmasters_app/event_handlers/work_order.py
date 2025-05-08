f59932132b1b1317d1b9d5a76e949fae964fb5d6
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-05-08 09:20:08 +0000

    Add feature to auto-inherit remarks and particulars for work order doctype

--- cardmasters_app/cardmasters_app/event_handlers/work_order.py
+++ cardmasters_app/cardmasters_app/event_handlers/work_order.py
@@ -1,15 +1,12 @@
 import frappe
 
-@frappe.whitelist()
-def inherit_remarks_particulars:
+def inherit_remarks_particulars(doc, method=None):
     if doc.sales_order:
         sales_order = frappe.get_doc("Sales Order", doc.sales_order)
         
         # Fetch values from Sales Order
         doc.custom_remarks = sales_order.custom_remarks
-        doc.custom_deadline = sales_order.custom_deadline
-        doc.custom_is_rush = sales_order.custom_is_rush
-
+        doc.custom_deadline = sales_order.delivery_date
         # Fetch the Sales Order Item description
         if doc.production_item:
             doc.custom_particulars = frappe.db.get_value(
