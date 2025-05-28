b650d21ea4dd1d61034bbe613288d40dae9d1782
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-05-28 08:15:44 +0000

    Fix purchase invoice displaying incorrect account after submission

--- cardmasters_app/public/js/purchase_invoice.js
+++ cardmasters_app/public/js/purchase_invoice.js
@@ -10,13 +10,6 @@ frappe.ui.form.on('Purchase Invoice', {
     },
   
     // runs when the form is loaded or refreshed
-    refresh(frm) {
-      if (frm.doc.custom_from_revolving_fund) {
-        frm.set_value(
-          'credit_to',
-          '2111 - ACCOUNT PAYABLE - RF - CM CDO'
-        );
-      }
-    }
+    
   });
   
