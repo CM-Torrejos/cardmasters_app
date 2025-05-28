fd842865dd185b350bddacf93dbacb9b8cc6fdea
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-05-28 07:30:37 +0000

    Add purchase invoice validation for petty cash

---
+++ cardmasters_app/public/js/purchase_invoice.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on('Purchase Invoice', {
+    // runs whenever the checkbox is toggled
+    custom_from_revolving_fund(frm) {
+      if (frm.doc.custom_from_revolving_fund) {
+        frm.set_value(
+          'credit_to',
+          '2111 - ACCOUNT PAYABLE - RF - CM CDO'
+        );
+      }
+    },
+  
+    // runs when the form is loaded or refreshed
+    refresh(frm) {
+      if (frm.doc.custom_from_revolving_fund) {
+        frm.set_value(
+          'credit_to',
+          '2111 - ACCOUNT PAYABLE - RF - CM CDO'
+        );
+      }
+    }
+  });
+  
