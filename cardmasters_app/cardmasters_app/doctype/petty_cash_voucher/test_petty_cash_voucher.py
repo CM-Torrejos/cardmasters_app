5bcd8a00669d62f6c79ffd711f27025094a9d07a
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-06-02 06:45:59 +0000

    Discontinue petty cash request and petty cash voucher features (use material request)

--- cardmasters_app/cardmasters_app/doctype/petty_cash_voucher/test_petty_cash_voucher.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Shan Torrejos and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPettyCashVoucher(FrappeTestCase):
-	pass
