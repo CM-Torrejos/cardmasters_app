0c2bf3533b53ae9fad07892c6e1723ea16a4b5de
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-08-11 17:15:01 +0800

    Set employee custom_id_code for naming series

---
+++ cardmasters_app/public/js/employee.js
@@ -0,0 +1,18 @@
+frappe.ui.form.on('Employee', {
+    validate: function(frm) {
+        if (frm.doc.date_of_joining && frm.doc.date_of_birth) {
+            // Parse the date_of_joining year (last two digits)
+            let doj = frappe.datetime.str_to_obj(frm.doc.date_of_joining);
+            let year_joining = doj.getFullYear().toString().slice(-2);
+
+            // Parse date_of_birth into mmddyy format
+            let dob = frappe.datetime.str_to_obj(frm.doc.date_of_birth);
+            let mm = String(dob.getMonth() + 1).padStart(2, '0'); // months are 0-based
+            let dd = String(dob.getDate()).padStart(2, '0');
+            let yy = dob.getFullYear().toString().slice(-2);
+
+            // Combine into the desired format: CM{YY}-{MMDDYY}
+            frm.set_value('custom_id_code', `CM${year_joining}-${mm}${dd}${yy}`);
+        }
+    }
+});
