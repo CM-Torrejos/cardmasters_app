eb40fe37b042d9d201cb17d952f5be268e69db24
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-08-26 10:29:34 +0800

    Export Fixtures

--- cardmasters_app/hooks.py
+++ cardmasters_app/hooks.py
@@ -9,6 +9,19 @@ app_license = "mit"
 fixtures = [
     # 1) Custom Fields 
     {
+        "doctype": "Role",
+        "filters": [
+            ["is_custom", "=", 1]   # only custom roles
+        ],
+        "sync_on_migrate": True,
+    },
+
+    # Custom Permissions (DocPerm)
+    {
+        "doctype": "Custom DocPerm",
+        "sync_on_migrate": True,
+    },
+    {
         "doctype": "Custom Field",
         "sync_on_migrate": True,
         "filters": [
