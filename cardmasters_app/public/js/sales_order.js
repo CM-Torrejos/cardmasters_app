17b06daff78563b88d1574ce0c0891f0c33718f0
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2025-11-11 13:17:09 +0800

    Fix oustanding balance and unviewable artist card

--- cardmasters_app/public/js/sales_order.js
+++ cardmasters_app/public/js/sales_order.js
@@ -68,9 +68,9 @@ frappe.ui.form.on('Sales Order', {
         //     frm.remove_custom_button('Update Items');
         // }
 
-		// if (frappe.user.has_role('CM Artist Head') || frappe.user.has_role('System Manager')) {
-        //     set_artist_card_button();
-        // }
+		if (frappe.user.has_role('CM Artist Head') || frappe.user.has_role('System Manager')) {
+            set_artist_card_button();
+        }
 
 		
 		// Optional: re-run it after status changes dynamically
