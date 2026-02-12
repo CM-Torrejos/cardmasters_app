e7f72d01b7304c84de4a611a68cb8315840123e3
CM-Reyes <creyes@cardmastersph.com>
2026-02-12 14:48:37 +0800

    feature: filter starred work orders using | Starred by | Like | % |, similar to frappe's Liked by.
    Before implementing this in prod, add a custom field in Work order:
    
    Label: Starred By
    Type: Small Text
    Hidden: 1

--- cardmasters_app/cardmasters_app/doctype/workstation_leader_log/workstation_leader_log.py
+++ cardmasters_app/cardmasters_app/doctype/workstation_leader_log/workstation_leader_log.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.model.document import Document
 
 
@@ -25,23 +26,42 @@ def get_starred_work_orders(names=None):
 
 @frappe.whitelist()
 def toggle_star(docname, state):
+    # Get current leader
     leader = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "custom_workstation_leader")
     if not leader:
-        frappe.throw("No Workstation Leader assigned to your Employee profile.")
+        frappe.throw("No Workstation Leader assigned to your profile.")
+
+    # Get current list from Work Order
+    # We use a 'FOR UPDATE' lock to prevent two leaders from clashing if they click at the same time
+    current_val = frappe.db.get_value("Work Order", docname, "custom_starred_by")
+    
+    try:
+        starred_by = json.loads(current_val) if current_val else []
+    except:
+        starred_by = []
 
     if state == "on":
-        # Create Log
-        doc = frappe.get_doc({
-            "doctype": "Workstation Leader Log",
-            "reference_document_type": "Work Order",
-            "reference_name": docname,
-            "workstation_leader": leader
-        })
-        doc.insert(ignore_permissions=True)
+        # Add to list if not there
+        if leader not in starred_by:
+            starred_by.append(leader)
+        
+        if not frappe.db.exists("Workstation Leader Log", {"reference_name": docname, "workstation_leader": leader}):
+            frappe.get_doc({
+                "doctype": "Workstation Leader Log",
+                "reference_document_type": "Work Order",
+                "reference_name": docname,
+                "workstation_leader": leader
+            }).insert(ignore_permissions=True)
+            
     else:
+        # Remove from list
+        if leader in starred_by:
+            starred_by.remove(leader)
+        
         # Delete Log
-        frappe.db.delete("Workstation Leader Log", {
-            "reference_name": docname,
-            "workstation_leader": leader
-        })
+        frappe.db.delete("Workstation Leader Log", {"reference_name": docname, "workstation_leader": leader})
+
+    # 3. Save the updated JSON string back to the Work Order
+    frappe.db.set_value("Work Order", docname, "custom_starred_by", json.dumps(starred_by), update_modified=False)
+            
     return "Success"
