df6de1526aaa9626476ba243c6237b27dd6cb584
CM-Reyes <creyes@cardmastersph.com>
2026-03-28 15:37:08 +0800

    feature: credit memo

---
+++ cardmasters_app/cardmasters_app/doctype/credit_memo_reason/credit_memo_reason.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Shan Torrejos and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Credit Memo Reason", {
+// 	refresh(frm) {
+
+// 	},
+// });
