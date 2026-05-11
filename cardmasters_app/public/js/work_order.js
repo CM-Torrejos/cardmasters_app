79f3b8e4ac940551b0e61324c57a53dba4046a51
CM Torrejos <208222831+CM-Torrejos@users.noreply.github.com>
2026-05-11 09:53:32 +0800

    Remove autofill for sales order in damages and return

--- cardmasters_app/public/js/work_order.js
+++ cardmasters_app/public/js/work_order.js
@@ -184,7 +184,6 @@ frappe.ui.form.on('Work Order', {
 		if (!frm.is_new() && !invalid_statuses.includes(frm.doc.status)) {
 			frm.add_custom_button(__('Issue Damages/Returns'), function() {
 				frappe.new_doc('Damages and Returns', {
-					sales_order:frm.doc.sales_order,
 					work_order: frm.doc.name,
 					date: 'Today',
 					date_of_damage_or_return: 'Today'
